6a9415cbbed50496bcb889693f9ca1529e02ad56
Manny Pissanos <mpissano@gmail.com>
2018-09-26 15:46:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.map do |holidays,supplies| 
-      holidays if supplies.include?("BBQ")
+      if supplies.include?("BBQ")
       end
     end.flatten
 end
