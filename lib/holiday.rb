965242711ff4dca32f95a7dd663625688600b377
Manny Pissanos <mpissano@gmail.com>
2018-09-26 15:15:20 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].each do |holidays, supplies|
     winter_arr << [holidays][supplies]
   end
-  winter_arr.merge
+  winter_arr
 end
     
 
