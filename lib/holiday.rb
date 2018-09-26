5a30a88edc666e51b73f105665825a6bf4e8c5de
Manny Pissanos <mpissano@gmail.com>
2018-09-26 15:43:13 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,8 +58,8 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  holiday_hash.each do |season, holiday|
-    holiday.map do {|supplies| holiday if supplies.include?("BBQ")}
+  holiday_hash.each do |season, holidays|
+    holidays.map do {|holidays,supplies| holidays if supplies.include?("BBQ")}
       end
     end
   holiday
